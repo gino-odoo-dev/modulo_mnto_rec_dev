84a4147e917978862e98f7d5037814850314366b
marcoacevedoginodev <macevedo@gino.cl>
2025-02-04 14:40:08 -0300

    input temporada y articulo

---
+++ rec_dev/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Ficha Tecnica',
+    'version': '1.0',
+    'summary': 'Modulo mantenimiento ficha tecnica creacion de recetas',
+    'description': 'Mantenimiento Ficha Tecnica.',
+    'author': 'MarcoAG',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/rec_model_views.xml',
+    ],
+    'images': ['static/description/icon.png'],  
+    'installable': True,
+    'application': True,
+}
+
---
+++ rec_dev/models/rec_model.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+class RecModel(models.Model):
+    _name = 'rec.model'
+    _description = 'Ficha Tecnica'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    product_id = fields.Many2one('product.template', string='Product')
+    temporada_id = fields.Many2one('cl.product.temporada', string='Temporada')
+
+
+class ArticuloModel(models.Model):
+    _name = 'rec.model'
+    _description = 'Articulo'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    product_id = fields.Many2one('product.template', string='Product')
+    temporada_id = fields.Many2one('cl.product.temporada', string='Temporada')
+
+
