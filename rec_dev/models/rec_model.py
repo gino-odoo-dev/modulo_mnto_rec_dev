4bb55365d403213274c05f8ccceee1854dc10d5b
marcoacevedoginodev <macevedo@gino.cl>
2025-02-05 12:17:11 -0300

    funcionalidad primera pantalla ok

--- rec_dev/models/rec_model.py
+++ rec_dev/models/rec_model.py
@@ -1,12 +1,29 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class RecModel(models.Model):
     _name = 'rec.model'
     _description = 'Ficha Tecnica'
 
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string='Name', required=False)
     description = fields.Text(string='Description')
     product_id = fields.Many2one('product.template', string='Product')
     temporada_id = fields.Many2one('cl.product.temporada', string='Temporada')
     articulo = fields.Char(string='Articulo', store=False)
 
+    state = fields.Selection([
+        ('draft', 'Borrador'),
+        ('next', 'Siguiente')
+    ], default='draft', string="Estado")
+
+    def next_button(self):
+        for record in self:
+            record.state = 'next'
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Mnto Ficha Tecnica',
+            'res_model': 'rec.model',
+            'view_mode': 'tree',
+            'view_id': self.env.ref('rec_dev.view_rec_model_tree').id,
+            'target': 'current',
+        }
+    
