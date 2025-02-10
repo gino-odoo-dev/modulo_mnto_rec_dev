af8822c18aeeae346901ec57aba5bddabe0753da
marcoacevedoginodev <macevedo@gino.cl>
2025-02-10 14:27:42 -0300

    reformateo de secuencia

--- rec_dev/models/receta.py
+++ rec_dev/models/receta.py
@@ -2,23 +2,34 @@ from odoo import models, fields, api
 
 class Temporada(models.Model):
     _name = 'temporada.model'
-    _description = 'Temporada' 
+    _description = 'Temporada'
+
+class Temporadas(models.Model):
+    _name = 'cl.product.temporada'
+    _description = 'Temporadas'
+
+class Secuencia(models.Model):
+    _name = 'secuencia_model'
+    _description = 'Secuencia'
+
+class Secuencias(models.Model):
+    _name = 'rec.secuencia'
+    _description = 'Secuencias'
+    _rec_name = 'name'
+
+    name = fields.Char(string="Nombre", required=True)
 
 class Componente(models.Model):
     _name = 'componente.model'
-    _description = 'Componente' 
+    _description = 'Componente'
 
 class Descripcion(models.Model):
     _name = 'descripcion.model'
-    _description = 'Descripcion' 
-
-class Secuencia(models.Model):
-    _name = 'secuencia.model'
-    _description = 'Secuencia'
+    _description = 'Descripcion'
 
 class Unimedida(models.Model):
     _name = 'unimedida.model'
-    _description = 'Unidad de Medida'    
+    _description = 'Unidad de Medida'
 
 class Depto(models.Model):
     _name = 'depto.model'
@@ -27,7 +38,7 @@ class Depto(models.Model):
 class Articulo(models.Model):
     _name = 'articulo.model'
     _description = 'Articulo'
-    _rec_name = 'nombre' 
+    _rec_name = 'nombre'
 
     nombre = fields.Char(string="Nombre", required=True)
 
@@ -51,10 +62,6 @@ class Campliado(models.Model):
     _name = 'campliado.model'
     _description = 'Costo Ampliado'
 
-class Temporadas(models.Model):
-    _name = 'cl.product.temporada'
-    _description = 'Temporadas'   
-
 class Receta(models.Model):
     _name = 'receta'
     _description = 'Receta'
@@ -63,13 +70,14 @@ class Receta(models.Model):
     temporada_id = fields.Many2one('temporada.model', string='Temporada', readonly=False)
     componente_id = fields.Many2one('componente.model', string='Componente', readonly=True)
     descripcion_id = fields.Many2one('descripcion.model', string='Descripcion', readonly=True)
-    secuencia_id = fields.Many2one('secuencia.model', string='Secuencia', readonly=True)
-    uni_medida_id = fields.Many2one('unimedida.model', string='Unidad de Medida', readonly=True)
+    secuencias_id = fields.Many2one('rec.secuencia', string='Secuencias', readonly=False)
+    secuencia_id = fields.Many2one('secuencia.model', string='Secuencia', readonly=False)
+    uni_medida_id = fields.Many2one('unimedida.model', string='UM', readonly=True)
     depto_id = fields.Many2one('depto.model', string='Departamento', readonly=True)
     articulo_id = fields.Many2one('articulo.model', string='Articulo', readonly=False)
-    comp_manu_id = fields.Many2one('compmanu.model', string='Compra o Manufacturado', readonly=True)
+    comp_manu_id = fields.Many2one('compmanu.model', string='C/M', readonly=True)
 
-    fact_perdida_id = fields.Float(string='Factor de Pérdida (%)', readonly=False)
+    fact_perdida_id = fields.Float(string='Factor de Perdida (%)', readonly=False)
     cantidad_id = fields.Integer(string='Cantidad', readonly=False)
     c_unitario_id = fields.Float(string='Costo Unitario', readonly=False)
     c_ampliado_id = fields.Float(string='Costo Ampliado', compute='calcular_costo_ampliado', store=True, readonly=True)
@@ -87,13 +95,13 @@ class Receta(models.Model):
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'receta',
-            'view_mode': 'tree,form',  
+            'view_mode': 'tree,form',
             'target': 'current',
             'context': {
-                'default_articulo_id': self.articulo_id.id, 
-                'search_default_articulo_id': self.articulo_id.id, 
+                'default_articulo_id': self.articulo_id.id,
+                'search_default_articulo_id': self.articulo_id.id,
             },
-            'domain': [('articulo_id', '=', self.articulo_id.id)], 
+            'domain': [('articulo_id', '=', self.articulo_id.id)],
         }
 
     @api.depends('cantidad_id', 'fact_perdida_id', 'c_unitario_id')
